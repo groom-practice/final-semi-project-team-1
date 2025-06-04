6ea1933da7652397dff26f5cf59b475fb27b6c6b
SNXWXH <snxwxh@gmail.com>
2025-06-04 11:04:37 +0900

    getPosts API 연동 및 더보기 기능 구현

--- components/PostItem.tsx
+++ components/PostItem.tsx
@@ -1,15 +1,12 @@
-export default function PostItem() {
+import { Post } from '@/type/posts';
+
+export default function PostItem({ post }: { post: Post }) {
   return (
     <div className='p-4 bg-white rounded shadow-sm'>
       <h1 className='font-semibold text-lg text-left cursor-pointer text-gray-900 hover:text-blue-600 transition-all duration-200'>
-        sunt aut facere repellat provident occaecati excepturi optio
-        reprehenderit
+        {post.title}
       </h1>
-      <p className='text-sm text-gray-600'>
-        quia et suscipit suscipit recusandae consequuntur expedita et cum
-        reprehenderit molestiae ut ut quas totam nostrum rerum est autem sunt
-        rem eveniet architecto
-      </p>
+      <p className='text-sm text-gray-600'>{post.body}</p>
     </div>
   );
 }
