df4616faf98c6d8f292c658ec38d5bc2a78b3836
gaeunchoi <gaeun1884@naver.com>
2025-06-04 13:55:32 +0900

    fix: Photos 동적 API route 수정

--- app/api/fetchImage/[id]/route.ts
+++ app/api/fetchImage/[id]/route.ts
@@ -2,9 +2,9 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(
   req: NextRequest,
-  { params }: { params: { id: string } }
+  { params }: { params: Promise<{ id: string }> }
 ) {
-  const { id } = params;
+  const { id } = await params;
 
   try {
     const res = await fetch(`https://picsum.photos/id/${id}/info`);
