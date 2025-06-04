2a0845034050e3cb2ea5a2aa0c401769464b0b22
gaeunchoi <gaeun1884@naver.com>
2025-06-04 18:21:30 +0900

    Feat: Photos 리스트, 아이템 모두 즐겨찾기 버튼 적용

--- app/photos/page.tsx
+++ app/photos/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import FavoriteBtn from "@/components/FavoriteBtn";
 import Spinner from "@/components/Spinner";
 import { Photo } from "@/type/photo";
 import Image from "next/image";
@@ -17,15 +18,22 @@ function PhotosList() {
   if (!photos) return <Spinner />;
 
   return (
-    <div className="grid sm:grid-cols-1 lg:grid-cols-3 place-items-center gap-3 p-6">
+    <div className="grid sm:grid-cols-1 lg:grid-cols-3 place-items-center gap-4 p-6">
       {photos.map((photo) => (
-        <div key={photo.id} className="">
+        <div
+          key={photo.id}
+          className="relative overflow-hidden hover:shadow-2xl transition-all duration-300"
+        >
+          <FavoriteBtn photo={photo} rightPos="right-2" />
           <Link href={`/photos/${photo.id}`}>
             <Image
               src={photo.download_url}
               alt={`${photo.id} Image`}
-              width={photo.width}
-              height={photo.height}
+              width={600}
+              height={600}
+              className="h-full object-cover object-center"
+              placeholder="blur"
+              blurDataURL={photo.download_url}
             />
           </Link>
         </div>
