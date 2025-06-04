ebcbae8f83dc58ccfe4cc78888abb4c7709742ae
gaeunchoi <gaeun1884@naver.com>
2025-06-04 13:56:43 +0900

    refactor: PhotoItem CustomHook 사용 및 컴포넌트 분리

--- app/photos/[id]/page.tsx
+++ app/photos/[id]/page.tsx
@@ -1,45 +1,21 @@
 "use client";
 
+import PhotoItem from "@/components/PhotoItem";
 import Spinner from "@/components/Spinner";
-import { Photo } from "@/type/photo";
-import Image from "next/image";
+import usePhotoItem from "@/hooks/usePhotoItem";
 import { useParams } from "next/navigation";
-import { useEffect, useState } from "react";
 
 function PhotoDetail() {
   const { id } = useParams();
-  const [photo, setPhoto] = useState<Photo | null>(null);
-  const [error, setError] = useState<string | null>(null);
-
-  useEffect(() => {
-    if (!id) return;
-
-    fetch(`/api/fetchImage/${id}`)
-      .then((res) => {
-        if (!res.ok) throw new Error("사진 상세정보를 불러오지 못했어용");
-        return res.json();
-      })
-      .then((data) => setPhoto(data))
-      .catch((err) => setError(err));
-  }, [id]);
+  const { photo, error } = usePhotoItem(id);
 
   if (error) return <p className="p-6 text-red-500">{error}</p>;
   if (!photo) return <Spinner />;
 
   return (
-    <div className="flex flex-row items-center justify-center gap-4 p-4">
-      <Image
-        src={photo.download_url}
-        alt={photo.id}
-        width={500}
-        height={500}
-        className="rounded-lg shadow-md"
-      />
-      <div className="h-full text-right p-2">
-        <p className="text-md">author by</p>
-        <p className="text-lg font-bold">{photo.author}</p>
-      </div>
-    </div>
+    <>
+      <PhotoItem photo={photo} />
+    </>
   );
 }
 
