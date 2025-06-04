b1cf1094780c113156710f3e5bc1d75944d77294
SNXWXH <snxwxh@gmail.com>
2025-06-04 10:31:55 +0900

    feat: postItem 컴포넌트 및 스크롤 적용

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,11 +1,26 @@
+import PostItem from '@/components/PostItem';
 import Toggle from '@/components/Toggle';
 
 export default function Post() {
   return (
-    <div className='flex mx-4'>
-      <div className='flex w-full h-9 justify-between items-center'>
+    <div className='flex flex-col mx-4 h-screen'>
+      <div className='flex w-full h-9 justify-between items-center mb-3'>
         <p className='text-2xl font-bold'>Posts List</p>
         <Toggle />
+      </div>
+      <div className='h-full space-y-4 bg-gray-100 p-4 rounded shadow overflow-y-auto'>
+        <PostItem />
+        <PostItem />
+        <PostItem />
+        <PostItem />
+        <PostItem />
+        <PostItem />
+        <PostItem />
+        <PostItem />
+        <PostItem />
+        <PostItem />
+        <PostItem />
+        <PostItem />
       </div>
     </div>
   );
