3f58a3f874bcca3f1696114470e064732984a002
SNXWXH <snxwxh@gmail.com>
2025-06-04 10:30:43 +0900

    feat: postItem 컴포넌트 생성

---
+++ components/PostItem.tsx
@@ -0,0 +1,15 @@
+export default function PostItem() {
+  return (
+    <div className='p-4 bg-white rounded shadow-sm'>
+      <h1 className='font-semibold text-lg text-left cursor-pointer text-gray-900 hover:text-blue-600 transition-all duration-200'>
+        sunt aut facere repellat provident occaecati excepturi optio
+        reprehenderit
+      </h1>
+      <p className='text-sm text-gray-600'>
+        quia et suscipit suscipit recusandae consequuntur expedita et cum
+        reprehenderit molestiae ut ut quas totam nostrum rerum est autem sunt
+        rem eveniet architecto
+      </p>
+    </div>
+  );
+}
